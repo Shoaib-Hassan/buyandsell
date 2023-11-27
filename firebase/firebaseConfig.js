8b31a02ab62ca727537d74e91414604db02ebf83
Shoaib-Hassan <121746914+Shoaib-Hassan@users.noreply.github.com>
2023-11-27 22:29:23 +0500

    Update firebaseConfig.js
    Updated

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnxnGmNvQicpDaP1wj7jTY1WWBZXqbJjk",
-  authDomain: "pubgm-id-buy-and-sell-a777d.firebaseapp.com",
-  databaseURL: "https://pubgm-id-buy-and-sell-a777d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pubgm-id-buy-and-sell-a777d",
-  storageBucket: "pubgm-id-buy-and-sell-a777d.appspot.com",
-  messagingSenderId: "998966989312",
-  appId: "1:998966989312:web:ab7121b41e7d60a6d6bee5"
+  apiKey: "YOURAPIKEY",
+  authDomain: "DATABASEDOMAIN",
+  databaseURL: "URL",
+  projectId: "PROJECTID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 if(!firebase.apps.length){
     firebase.initializeApp(firebaseConfig)
